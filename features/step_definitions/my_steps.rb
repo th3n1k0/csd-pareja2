cdc4d416ed51adcf6282fa156b05e3694a886538
Daniel Ccopa <daniel.ccopa.vilca@gmail.com>
2016-12-16 20:34:23 +0000

    integracion 2

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,14 +6,10 @@ Then(/^debo ver "(.*?)"$/) do |parametro|
   expect(page.body).to match /#{parametro}/m
 end
 
+When(/^defino a "([^"]*)" como jugador$/) do |jugador|
+  fill_in("jugador", :with => "Thiago") 
+end
+
 When(/^inicio una partida$/) do
   click_button("iniciar")
 end
-
-
-
-# When(/^defino a "([^"]*)" como jugador$/) do |jugador|
-#   visit '/'
-#   fill_in("jugador", :with => "Thiago") 
-#   click_button("iniciar")
-# end
